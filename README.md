4074ce74cc93cbee30219459dbce7d1e2c73957c
daragh <daraghdownes12@gmail.com>
2024-06-16 09:41:30 +0100

    remove sudo, update run.sh

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Ansible playbook for my linux setup  
 
-```sudo wget -O - https://raw.githubusercontent.com/daraghd/linux-setup/main/run.sh | sudo bash```
+```wget -O - https://raw.githubusercontent.com/daraghd/linux-setup/main/run.sh | bash```
--- run.sh
+++ run.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
 printf "Linux setup\n \n"
+
 echo "Installing git, ansible and updating"
  if [ -x "$(command -v apt)" ]; then
      sudo apt upgrade 
-     sudo apt install -y --needed git ansible
+     sudo apt install -y git ansible
  elif [ -x "$(command -v pacman)" ]; then
      sudo pacman -Syu --noconfirm
-     sudo pacman -S --noconfirm git ansible
+     sudo pacman -S --noconfirm --needed git ansible
  else
      echo "Not arch or ubuntu."
      exit 1
  fi
 
 mkdir ~/.linux-setup
-git clone https://github.com/daraghd/linux-setup ~/.linux-setup
+cd ~/.linux-setup
+git pull
+
+if [ $? -eq 0 ]; then
+    echo "Repository already made"
+    echo "Run update.sh instead"
+    exit 1
+else
+    echo "Cloning"
+    git clone https://github.com/daraghd/linux-setup ~/.linux-setup
+fi
+
 ansible-playbook ~/.linux-setup/main.yml
+
+
